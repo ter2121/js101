ba52386dcca8cf11513612b5db209be0f7e7238d
ter2121 <ter2121@columbia.edu>
2021-03-10 23:38:48 -0800

    Day 3 Final Commit Easy 2 Started

---
+++ smallProblems/Easy 2/easy2_4.js
@@ -0,0 +1,21 @@
+function square(num1) {
+  console.log(num1 * num2);
+ }
+
+
+square(-8);
+
+
+/*
+Using multiply()
+
+function multiply(num1, num2) {
+  return num1 * num2;
+ };
+
+
+function square (num) {
+  multiply(num, num);
+}
+
+*/
