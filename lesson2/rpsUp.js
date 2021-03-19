65295e81aeb5f5945757f07a9e430bd70d32a8cd
Eddie Revells <ter2121@columbia.edu>
2021-03-19 15:30:47 -0700

    Day 7 RPS Update

--- lesson2/rpsUp.js
+++ lesson2/rpsUp.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const lineDecoration = '<--$--$--$--$--$--$--$--$--$--$--$--$--$--$-->';
 let userScore = 0
 let computerScore = 0
 
@@ -47,35 +48,46 @@ function matchScoreTotal(result){
       computerScore += 1;
       break;
   }
-  console.log(`The score is ${userScore} to ${computerScore}.`);
+  prompt(`The score is ${userScore} to ${computerScore}.`);
 }
 
 function matchOver(userScoreTotal, computerScoreTotal) {
   if (userScoreTotal === 5) {
-    console.log(`Match over. You have won ${userScoreTotal} games.`)
+    prompt(`\n ${lineDecoration}\n  Match over. You have won ${userScoreTotal} games.\n ${lineDecoration}`)
     userScore = 0;
     computerScore = 0;
   } else if (computerScoreTotal === 5) {
-    console.log(`Match over. The computer has won ${computerScoreTotal} games.`);
+    prompt(`\n ${lineDecoration}\n  Match over. The computer has won ${computerScoreTotal} games. \n ${lineDecoration}`)
     userScore = 0;
     computerScore = 0;
   }
 }
 
-while (true) {
+
+ 
+function userChoiceFunction() {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
     choice = readline.question();
-  }
+  } 
+  return choice;
+}
 
+function computerChoiceFunction() {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
+  return computerChoice
+}
 
-  displayGameWinner(choice, computerChoice);
+
+while (true) {
+
+  displayGameWinner(userChoiceFunction(), computerChoiceFunction());
   matchOver(userScore, computerScore);
+
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
@@ -87,4 +99,5 @@ while (true) {
   if (answer[0] !== 'y') break;
 
   matchScoreTotal();
+  
 }
