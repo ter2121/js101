d9d1eb99400db7f01a43699e1223b35e1054965a
Eddie Revells <ter2121@columbia.edu>
2021-03-28 17:35:51 -0700

    Day 12 update

--- smallProblems/Easy 3/easy3_10.js
+++ smallProblems/Easy 3/easy3_10.js
@@ -9,3 +9,8 @@ greater than 0 AD
 
 input : integer 
 output string + stringEnd
+
+//pseudocode
+// centuryConversion = number / 100
+// if last numbers != 0 Math.ceil(century) + stringEnd
+// if last number = 0 Math.floor(number) + stringEnd
